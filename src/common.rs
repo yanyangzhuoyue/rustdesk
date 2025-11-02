41473857e19698bf305a8d845ae0838bda040b79
yanyangzhuoyue <1418134850@qq.com>
2025-11-02 17:24:22 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://desk.easyuser.top".to_owned()
+    "http://desk.easyuser.top:21114".to_owned()
 }
 
 #[inline]
