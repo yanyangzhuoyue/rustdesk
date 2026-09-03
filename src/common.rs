c3b964dd3bb55671caf4826328f0b0f78626d747
yanyangzhuoyue <1418134850@qq.com>
2026-09-03 08:00:06 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -1137,7 +1137,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://remote.easyuser.top".to_owned()
 }
 
 #[inline]
